f2d561280ade7e830833457f323b5021f80a1bf6
Chitiva09 <bermeocamilo12@gmail.com>
2024-07-23 17:40:50 -0500

    list

--- camilo2/src/main/java/com/cursos/camilo2/controller/CursosController.java
+++ camilo2/src/main/java/com/cursos/camilo2/controller/CursosController.java
@@ -1,17 +1,39 @@
 package com.cursos.camilo2.controller;
 
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cursos.camilo2.cursomodel.Curso;
 
+import jakarta.annotation.PostConstruct;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 @RestController
 public class CursosController {
-    @GetMapping(value = "curso", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Curso getCurso(){
-        System.out.println("Entro al controller");
-        return new Curso("Java", 100,"Mañana");
+
+    private List<Curso> cursos;
+
+    @PostConstruct
+    public void init() {
+        cursos = new ArrayList<>();
+        cursos.add(new Curso("spring", 30, "tarde"));
+        cursos.add(new Curso("java", 30, "mañana"));
+        cursos.add(new Curso("spring boot", 30, "noche"));
+        cursos.add(new Curso("pyton", 30, "noche"));
+        cursos.add(new Curso("go", 30, "tarde"));
+
     }
+
+    @GetMapping(value = "cursos", produces = MediaType.APPLICATION_JSON_VALUE)
+    private List<Curso> getCursos() {
+        return cursos;
+    }
+
+
 
 }
